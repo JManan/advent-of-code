379be61bd2af0a66fa66a28c2a6934c37d884721
JManan <manan.jain.0912@gmail.com>
2022-12-05 18:02:20 +0530

    1st part

--- check.py
+++ check.py
@@ -1,6 +1,6 @@
-h = "16-80,80-87"
-g = (h.split(','))
-k = g[0].split('-')
-f = g[1].split('-')
-one, two, three, four = int(k[0]), int(k[1]), int(f[0]), int(f[1])
-print(one)
+empty= []
+empty = ''.join([str(elem) for elem in empty])
+print(type(empty))
+print((empty))
+empty = list(empty)
+print(type(empty))
---
+++ day5.py
@@ -0,0 +1,43 @@
+index = {
+    '1':'ftnzmghj',
+    '2':'jwv',
+    '3':'htbjlvg',
+    '4':'lvdcnjpb',
+    '5':'grpmswf',
+    '6':'mvnbfchg',
+    '7':'rmghd',
+    '8':'dzvmnh',
+    '9':'hfng',
+}
+
+# index = {
+#     '1':'nz',
+#     '2':'dcm',
+#     '3':'p',
+# }
+
+with open(r'input2.txt', 'r') as input_file:
+    for line in input_file:
+        line = line[:-1]
+        line = list(line.split(" "))
+        line.pop(0)
+        line.pop(1)
+        line.pop(2)
+        # print(index['1'])
+        # print(index['2'])
+        l = index[line[1]][:int(line[0])]
+        l = l[::-1]
+        # if index[]
+        # print(l)
+        index[line[2]] = l + index[line[2]]
+        index[line[1]] = list(index[line[1]])
+        for i in range(int(line[0])):
+            print(type(index[line[1]]))  # showing list and string data type
+            if (index[line[1]] != []):  
+                index[line[1]].pop(0)
+        index[line[1]] = ''.join([str(elem) for elem in index[line[1]]])
+
+for i in range(1, 10):
+    print(index[str(i)][0])
+
+# index['1'].strip
